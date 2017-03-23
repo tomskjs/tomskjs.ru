2896241ba21f4a16b9cce1dbbc6febcd7a73e8df
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-03-23 18:31:46 +0300

    Add cycle.js hello world

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import { TomskJSLogo } from './components/tomskjs_logo/index'
+import { run, FantasyObservable, Drivers } from '@cycle/run'
+import { VNode, DOMSource, div, makeDOMDriver } from '@cycle/dom'
+import xs, { Stream } from 'xstream'
 
-console.log('logo', TomskJSLogo())
+interface Sources {
+  DOM: DOMSource,
+}
+
+interface Sinks {
+  DOM: Stream<VNode>,
+  [key: string]: FantasyObservable,
+}
+
+function main(): Sinks {
+  return {
+    DOM: xs.of(div('hello world')),
+  }
+}
+
+const drivers: Drivers<Sources, Sinks> = {
+  DOM: makeDOMDriver('#app'),
+}
+
+run(main, drivers)
