6ec025cd83125ec6a09936c1c5953e94c50a71fa
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-03-23 20:57:24 +0300

    Add descriptions

--- src/pages/main_page/index.ts
+++ src/pages/main_page/index.ts
@@ -8,19 +8,39 @@ import { Layout } from '../../components/layout'
 import { TomskJSLogo } from '../../components/tomskjs_logo'
 import * as styles from './styles.css'
 
-const description = `
-  У тебя не должно быть любимого оружия. Стать слишком близким с одним
-  оружием — большая ошибка, чем недостаточное знание его. Не копируй других,
-  действуй по ситуации. Плохо также для солдат и командиров испытывать к кому-то
-  любовь или неприязнь. Тщательно изучи это.
-`
+
+function About() {
+  const text = `
+    У тебя не должно быть любимого оружия. Стать слишком близким с одним
+    оружием — большая ошибка, чем недостаточное знание его. Не копируй других,
+    действуй по ситуации. Плохо также для солдат и командиров испытывать к кому-то
+    любовь или неприязнь. Тщательно изучи это.
+  `
+
+  return div([
+    div('.' + styles.logo, TomskJSLogo()),
+    div('.' + styles.heading, Title('TomskJS', { size: 'L' })),
+    div('.' + styles.headerText, Text(text)),
+  ])
+}
+
+
+function DescriptionSection(title: string, text: string) {
+  return div('.' + styles.description, [
+    div('.' + styles.title, Title(title, { size: 'S' })),
+    div('.' + styles.text, Text(text)),
+  ])
+}
+
 
 export function MainPage(_: Sources): Sinks {
   const vtree$ = xs.of(
     Layout([
-      div('.' + styles.logo, TomskJSLogo()),
-      div('.' + styles.heading, Title('TomskJS', { size: 'L' })),
-      div('.' + styles.headerText, Text(description)),
+      About(),
+      div('.' + styles.descriptions, [
+        DescriptionSection('Наши цели', 'Стратегия Ити отличается от других систем тем, что если ты хоть немного уклонишься с верного Пути, то подвергнешься опасности и попадешь на скользкую дорогу. Если ты будешь просто перелистывать эту книгу, ты не постигнешь Пути Стратегии. Впитай сказанное. Не просто читай, запоминая или имитируя, но напряженно изучай, чтобы вобрать знания в свое существо и ощутить принцип собственным сердцем.'),
+        DescriptionSection('Участие', 'Рассчитывай все. Расчет в Стратегии невозможно изучить без большого объема практики.'),
+      ]),
     ]),
   )
   return {
