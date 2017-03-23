45fbbb07c6e86d0aab01b0fc3aaedb7661f7aca9
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-03-23 19:47:02 +0300

    make svg logo work

--- src/components/tomskjs_logo/index.ts
+++ src/components/tomskjs_logo/index.ts
@@ -1,4 +1,10 @@
+import { svg } from '@cycle/dom'
 
 export function TomskJSLogo() {
-  return require('./logo.svg');
+  const attrs = {
+    width: 200,
+    height: 200,
+    viewBox: '0 0 200 200',
+  }
+  return svg({ attrs, props: { innerHTML: require('./logo.svg') } })
 }
